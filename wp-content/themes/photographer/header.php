c6098c096d19a40e21290ba5163b3bd434f93333
Inbound Now <david@inboundnow.com>
2015-03-21 12:40:57 -0700

    bae

--- wp-content/themes/photographer/header.php
+++ wp-content/themes/photographer/header.php
@@ -3,19 +3,19 @@
 	{
 		header( 'X-UA-Compatible: IE=edge,chrome=1' );
 	}
-	
-	
+
+
 	$home_landing = "";
-	
+
 	if ( is_page_template( 'template-homepage_landing.php' ) )
 	{
 		$home_landing = 'home-landing';
 	}
-	
-	
+
+
 	$no_fludibox = "";
 	$pixelwars__fluidbox = get_option( 'pixelwars__fluidbox', 'Yes' );
-	
+
 	if ( $pixelwars__fluidbox == 'No' )
 	{
 		$no_fludibox = 'no-fludibox';
@@ -27,10 +27,10 @@
 
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	
+
 	<?php
 		$mobile_zoom = get_option( 'mobile_zoom', 'Yes' );
-		
+
 		if ( $mobile_zoom == 'No' )
 		{
 			?>
@@ -44,26 +44,26 @@
 			<?php
 		}
 	?>
-	
+
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	
+
 	<?php
 		wp_head();
 	?>
 </head>
 
 <body <?php body_class(); ?>>
-	
+
     <div id="page" class="hfeed site">
         <header id="masthead" class="site-header" role="banner">
 			<div class="site-logo">
 				<?php
 					$logo_type = get_option( 'logo_type', 'Text Logo' );
-					
+
 					if ( $logo_type == 'Image Logo' )
 					{
 						$logo_image = get_option( 'logo_image', "" );
-						
+
 						?>
 							<h1 class="site-title">
 								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
@@ -75,7 +75,7 @@
 					else
 					{
 						$select_text_logo = get_option( 'select_text_logo', 'WordPress Site Title' );
-						
+
 						if ( $select_text_logo == 'Theme Site Title' )
 						{
 							$text_logo_out = stripcslashes( get_option( 'theme_site_title', "" ) );
@@ -84,26 +84,26 @@
 						{
 							$text_logo_out = get_bloginfo( 'name' );
 						}
-						
+
 						?>
 							<h1 class="site-title">
 								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo $text_logo_out; ?></a>
 							</h1>
 						<?php
-						
-						
+
+
 						$select_tagline = get_option( 'select_tagline', 'WordPress Tagline' );
-						
+
 						if ( $select_tagline == 'WordPress Tagline' )
 						{
 							?>
-								<p class="site-description">vignettes of the bay</p>
+								<p class="site-description">vignettes of the bae</p>
 							<?php
 						}
 						else
 						{
 							$theme_tagline = stripcslashes( get_option( 'theme_tagline', "" ) );
-							
+
 							if ( $theme_tagline != "" )
 							{
 								?>
@@ -114,12 +114,12 @@
 					}
 				?>
 			</div>
-			
-			
+
+
 			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
-				
+
 				<a class="menu-toggle"><span class="lines"></span></a>
-				
+
 				<div class="nav-menu">
 					<?php
 						wp_nav_menu( array( 'theme_location' => 'pixelwars_theme_menu_location_1',
